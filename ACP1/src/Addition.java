edd43c0212bc99b5065e6bbcdb06e28bf26e4569
PriceJeremiah11B <52722550+PriceJeremiah11B@users.noreply.github.com>
2019-11-21 21:20:45 -0700

    55

--- ACP1/src/Addition.java
+++ ACP1/src/Addition.java
@@ -1,4 +1,10 @@
-
-public class Addition {
-
+public class Addition implements doMath {
+	public void execute(Integer input1, Integer input2){
+		Integer addition = input1 + input2;
+		System.out.println(addition);
+	}
+	
+	public int addition(int i, int j) {
+		return i + j;
+	}
 }
--- Discounts/src/calculator/App.java
+++ Discounts/src/calculator/App.java
@@ -1,10 +1,19 @@
 package calculator;
+import java.util.Scanner;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		try {
+			Scanner input = new Scanner(System.in);
+			Controller calc = new Controller();
+			
+			String input1;
+			Integer input2;
+		}
+		
+		finally{};
 	}
 
 }
